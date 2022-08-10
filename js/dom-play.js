715d95a5c55810043c06402334023b0c32fe1304
julianaflorence <julianapuyallup@gmail.com>
2022-08-10 00:12:24 -0700

    fixed

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,39 +1,43 @@
 //alert("Hi there!");
 function myAlert(){
-    alert("Im clicked");
+    alert("Imclicked");
+}
+
+const spans = document.querySelectorAll("#play span");
+
+
+console.log(spans);
+
+ for ( const mySpan of spans ){
+     //mySpan.addEventListener("click",);
+     mySpan.addEventListener("click",function(ev)
+     {  highlightActor(mySpan.dataset.actor);
+    
+    
+    });
+    // alert ("the current actor is"+" "+mySpan.dataset.actor);
+   
  }
-    const spans = document.querySelector("#play span");
 
-    console.log(spans);
-
-        for(const mySpan of spans){
-            //   mySpan.addEventListener("click", myAlert);
-            //alert(mySpan.dataset.actor);
-        mySpan.addEventListener("click",function(ev){ 
-             // alert("the current actor is " + mySpan.dataset.actor);
-         highlightActor(mySpan.dataset.actor);
-             });
-
-                }
- 
-function highlightActor(actor){
+ function highlightActor(actor){
 
     for(const mySpan of spans){
+        if(actor == mySpan.dataset.actor){//current actor, highlight
+        mySpan.style.backgroundColor='yellow'
 
-         }
-    }
-     
-    if(actor == mySpan.dataset.actor){
-        mySpan.style.backgroundColor ='yellow';
-    } else{
-        mySpan.style.backgroundColor='white';
-    }
-
-   function highlight(el){
-       
-       if(el.style.backgroundColor =='yellow'){
-            el.style.backgroundColor ='white';
-             } else{
-                    el.style.backgroundColor='yellow';
+        }else{// other actor, background white
+            mySpan.style.backgroundColor='orange'
         }
     }
+ 
+
+
+function highlight(el){
+    if( el.style.backgroundColor == 'yellow'){
+    el.style.backgroundColor='blue';
+    }else{
+        el.style.backgroundColor='yellow'
+    }
+
+}
+}
